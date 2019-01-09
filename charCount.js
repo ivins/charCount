887805f72738a95bfad2b3daa190a53b418759f3
Clive <seelive.i@gmail.com>
2019-01-09 19:53:53 +0000

    function working

--- charCount.js
+++ charCount.js
@@ -1,5 +1,18 @@
-var countLetters = function() {
+var countLetters = function(sentence) {
+  var arr = sentence.replace(/\s/g, "").toLowerCase().split("");
+  var final = {};
 
-}
+  for (var num in arr){
+    var current = arr[num];
+    final[current] = 0;
 
-countLetters("Lighthouse in the House");
+    for (var x in arr) {
+      if (current === arr[x]) {
+        final[current]++;
+      }
+    }
+  }
+  return final;
+};
+
+console.log(countLetters("Lighthouse in the House"));
